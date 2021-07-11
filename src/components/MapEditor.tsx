5decafb5a7759d2fd6a1b3befd193df257e4a68b
andrewyohanes123 <andrew.yohanes123@gmail.com>
2021-07-12 07:12:22 +0800

    satellite imagery

--- src/components/MapEditor.tsx
+++ src/components/MapEditor.tsx
@@ -5,7 +5,7 @@ import { Map } from '../pages/MainPage';
 const { REACT_APP_IP_ADDRESS, REACT_APP_PORT }: NodeJS.ProcessEnv = process.env;
 export const MapEditor: FC = (): ReactElement => {
   const center: [number, number] = useMemo(() => ([125.12217564443125, 1.4406812288395177]), []);
-  const zoom: [number] = useMemo(() => ([13]), []);
+  const zoom: [number] = useMemo(() => ([15]), []);
 
   return (
     <div style={{ height: '100%' }}>
@@ -13,9 +13,9 @@ export const MapEditor: FC = (): ReactElement => {
         // eslint-disable-next-line
         style={{
           sources: {
-            'land': {
-              type: 'vector',
-              url: `${REACT_APP_IP_ADDRESS}:${REACT_APP_PORT}/map/base`,
+            land: {
+              type: 'geojson',
+              data: `${REACT_APP_IP_ADDRESS}:${REACT_APP_PORT}/map/base`,              
             },
           },
           layers: [
@@ -27,16 +27,21 @@ export const MapEditor: FC = (): ReactElement => {
               }
             },
             {
-              id: 'tc-land',
+              id: 'land',
               source: 'land',
-              'source-layer': 'land',
+              // 'source-layer': 'land',
               type: 'fill',
               paint: {
-                'fill-color': '#ffffff',
-              }
-            }
+                'fill-color': '#dfdfdf',
+                'fill-opacity': 1,
+              },
+              layout: {
+                visibility: 'visible'
+              },
+              // minzoom: 13  
+            },
           ],
-          name: 'empty',
+          name: 'EMPTY',
           version: 8,
         }}
         center={center}
