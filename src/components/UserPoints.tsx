c28424bfc2691ee5e4d7e351c1e5abf99be85274
andrewyohanes123 <andrew.yohanes123@gmail.com>
2022-07-25 12:09:36 +0800

    (*) working map rendering

--- src/components/UserPoints.tsx
+++ src/components/UserPoints.tsx
@@ -1,12 +1,20 @@
-import { FC, ReactElement, useContext, useCallback, useState, useRef, useEffect } from 'react'
-import { Toast } from 'primereact/toast'
+import {
+  FC,
+  ReactElement,
+  useContext,
+  useCallback,
+  useState,
+  useRef,
+  useEffect,
+} from "react";
+import { Toast } from "primereact/toast";
 // import axios from 'axios'
 // import {  } from 'mapbox-gl'
-import { MapInstance } from '../contexts/MapInstanceContext'
-import { ModelsContext } from '../contexts/ModelsContext';
-import { Point } from '../types/Types'
+import { MapInstance } from "../contexts/MapInstanceContext";
+import { ModelsContext } from "../contexts/ModelsContext";
+import { Point } from "../types/Types";
 // import { svgColor } from '../modules/SVGColorizer';
-import { UserContext } from '../contexts/UserContext';
+import { UserContext } from "../contexts/UserContext";
 
 // const { REACT_APP_IP_ADDRESS, REACT_APP_PORT }: NodeJS.ProcessEnv = process.env;
 
@@ -14,7 +22,10 @@ export const UserPoints: FC = (): ReactElement => {
   const { map } = useContext(MapInstance);
   const { models } = useContext(ModelsContext);
   const { user } = useContext(UserContext);
-  const [points, setPoints] = useState<{ rows: Point[], count: number }>({ rows: [], count: 0 });
+  const [points, setPoints] = useState<{ rows: Point[]; count: number }>({
+    rows: [],
+    count: 0,
+  });
   // const [types, setTypes] = useState<Type[]>([]);
   // const [imageLoaded, toggleImageLoaded] = useState<boolean>(false);
   const { Point } = models!;
@@ -22,25 +33,34 @@ export const UserPoints: FC = (): ReactElement => {
 
   const getPoints = useCallback(() => {
     Point.collection({
-      attributes: ['name', 'pointType', 'properties', 'geometry', 'description', 'type_id'],
-      include: [
-        { model: 'Type', attributes: ['name', 'color', 'icon'] }
+      attributes: [
+        "name",
+        "pointType",
+        "properties",
+        "geometry",
+        "description",
+        "type_id",
       ],
+      include: [{ model: "Type", attributes: ["name", "color", "icon"] }],
       where: {
-        user_id: user?.id
-      }
-    }).then(resp => {
-      setPoints({ rows: resp.rows as Point[], count: resp.count });
-    }).catch(e => {
-      toast.current?.show({ severity: 'error', summary: 'Terjadi Kesalahan', detail: e.toString() });
+        user_id: user?.id,
+      },
     })
+      .then((resp) => {
+        setPoints({ rows: resp.rows as Point[], count: resp.count });
+      })
+      .catch((e) => {
+        toast.current?.show({
+          severity: "error",
+          summary: "Terjadi Kesalahan",
+          detail: e.toString(),
+        });
+      });
   }, [Point, user]);
-
 
   useEffect(() => {
     getPoints();
   }, [getPoints]);
-
 
   // useEffect(() => {
   //   if (types.length > 0 && points.rows.length > 0 && typeof map !== 'undefined') {
@@ -76,38 +96,63 @@ export const UserPoints: FC = (): ReactElement => {
   // }, [types, points, map]);
 
   useEffect(() => {
-    if (typeof map !== 'undefined' && points.rows.length > 0) {
-      const currentSource = map.getSource('points');
-      const currentLayer = map.getLayer('points');
-      (typeof currentLayer !== 'undefined') && map.removeLayer('points');
-      (typeof currentSource !== 'undefined') && map.removeSource('points');
+    if (typeof map !== "undefined" && points.rows.length > 0) {
+      const currentSource = map.getSource("points");
+      const currentLayer = map.getLayer("points");
+      typeof currentLayer !== "undefined" && map.removeLayer("points");
+      typeof currentSource !== "undefined" && map.removeSource("points");
 
-      map.addSource('points', {
-        type: 'geojson',
+      const sourcePoints = points.rows.map((point) => point.geometry.features);
+      console.log(sourcePoints);
+
+      map.addSource("points", {
+        type: "geojson",
         // @ts-ignore
-        data: points.rows.map(point => point.geometry)
+        data: {
+          type: "FeatureCollection",
+          features: sourcePoints.flat(),
+        },
       });
 
       map.addLayer({
-        'id': 'points',
-        type: 'fill',
-        source: 'points',
-        layout: {
-        },
+        id: "points",
+        type: "fill",
+        source: "points",
+        layout: {},
         paint: {
-          'fill-color': 'rgba(0, 184, 148, 0.6)',
-          'fill-outline-color': 'rgba(0, 184, 148, 0.5)'
-        }
+          "fill-color": [
+            "interpolate",
+            ["linear"],
+            ["number", ["get", "damage_percentage"]],
+            0,
+            ["rgba", 29, 209, 161, 0.25],
+            50,
+            ["rgba", 249, 202, 36, 0.25],
+            100,
+            ["rgba", 235, 77, 75, 0.25],
+          ],
+          // ["rgba", ["get", "damage_percentage"], 0, ["-", 100, ["get", "damage_percentage"]]],
+          "fill-outline-color": [
+            "interpolate",
+            ["linear"],
+            ["number", ["get", "damage_percentage"]],
+            0,
+            ["rgba", 29, 209, 161, 0.35],
+            50,
+            ["rgba", 249, 202, 36, 0.35],
+            100,
+            ["rgba", 235, 77, 75, 0.35],
+          ],
+        },
       });
-
     }
     return () => {
       // if (typeof map !== 'undefined') {
       //   map.removeLayer("points");
       //   map.removeSource("points");
       // }
-    }
-  }, [map, points])
+    };
+  }, [map, points]);
 
   // useEffect(() => {
   //   if (points.rows.length > 0 && types.length > 0) {
@@ -118,5 +163,5 @@ export const UserPoints: FC = (): ReactElement => {
     <div>
       <Toast ref={toast} />
     </div>
-  )
-}
+  );
+};
